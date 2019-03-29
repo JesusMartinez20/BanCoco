0a7cbc83dab4af047b6be8b937bf3fe25499f623
Jesús Martínez Cortez <38960100+JesusMartinez20@users.noreply.github.com>
2019-03-29 17:45:31 -0600

    Component header done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MatComponentsModule } from './mat-components/mat-components.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  BrowserModule,
+    AppRoutingModule,
+    MatComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
