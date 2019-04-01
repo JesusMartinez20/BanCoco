946ae4faf8a88301d3c95e8f6509892365d36e0d
ChelohHolmes <marcelogerstein@gmail.com>
2019-04-01 00:18:05 -0600

    Minor changes. Conflict on transactions component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-//import { TransactionsComponent } from './transactions/transactions.component';
+import {TransactionsComponent} from './transactions/transactions.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
+  // { path: 'Transactions', component: TransactionsComponent },
 ];
 
 @NgModule({
